b9471c4a33eb40eb1122b0cd3d9baa291cddcc20
Lucas Burgett <lucasaburgett@gmail.com>
2026-02-22 16:59:26 -0800

    New test file

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print('hello')
-def hello(): pass
+import requests
+
+api_key = "sk-abc123456"
+
+def get_data():
+    r = requests.get("https://api.example.com/data")
+    return r.json()
+
+def risky():
+    try:
+        get_data()
+    except:
+        pass
